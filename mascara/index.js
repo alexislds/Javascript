2e47de6eef44d8f861cb3dda6ece2482bdd4e08d
Alexis Santos <alexis.luiz.dias@gmail.com>
2016-12-16 01:09:05 -0200

    MASCARA: FUNCIONANDO IGUAL BRUNO QUER

--- mascara/index.js
+++ mascara/index.js
@@ -6,39 +6,39 @@ var padrao = {
   cnpj : '##.###.###/####-##',
 };
 
-var $input = document.querySelector('#input');
+window.addEventListener('keyup', function(evento) {
+  var alvo = evento.target;
+  if (alvo.tagName == 'INPUT' && alvo.hasAttribute('data-mascara')) {
+    var alvoAtt = eval(alvo.getAttribute('data-mascara'));
 
-$input.addEventListener('keyup', function(evento){
-
-  if (evento.keyCode != 8) {
-    mascara(padrao.telCel, 'numero');
+    mascara(alvo, alvoAtt, 'numero');
   }
 });
 
 
 
-function mascara(padrao, tipo){
-  var valor = $input.value;
+function mascara(input, padrao, tipo){
+  var valor = input.value;
 
-  $input.setAttribute('maxlength', padrao.length);
+  input.setAttribute('maxlength', padrao.length);
 
   if (tipo == 'numero') {
-    $input.value = valor.replace(/[A-Z]/gi, '');
+    input.value = valor.replace(/[A-Z]/gi, '');
   }
   else if (tipo == 'texto') {
-    $input.value = valor.replace(/[0-9]/gi, '');
+    input.value = valor.replace(/[0-9]/gi, '');
   }
 
-  if ($input.value != '') {
+  if (input.value != '') {
     for (i = 0; i < valor.length; i++) {
-      valor = $input.value;
+      valor = input.value;
 
       if (padrao[i] != '#' && valor[i] != padrao[i]) {
         var novaString = padrao[i] + valor[i];
         var tudoAntes = valor.slice(0, i);
         var tudoDepois = valor.slice(i+1, padrao.length);
         var novoValor = tudoAntes.concat(novaString).concat(tudoDepois);
-        $input.value = novoValor;
+        input.value = novoValor;
       }
     };
   }
