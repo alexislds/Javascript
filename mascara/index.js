995ccb3dabeb7322cd4416a8fe4bbc23472b255a
Alexis Santos <alexis.luiz.dias@gmail.com>
2016-12-22 17:54:48 -0200

    teste

--- mascara/index.js
+++ mascara/index.js
@@ -1,54 +1,58 @@
 var padrao = {
-  telRes : {regra : '(##) ####-####',     tipo: 'numero'},
-  telCel : {regra : '(##) #####-####',    tipo: 'numero'},
-  cpf    : {regra : '###.###.###-##',     tipo: 'numero'},
-  data   : {regra : '##/##/####',         tipo: 'numero'},
-  cnpj   : {regra : '##.###.###/####-##', tipo: 'numero'}
+  telRes : {regra : '(##) ####-####'},
+  telCel : {regra : '(##) #####-####'},
+  cpf    : {regra : '###.###.###-%%'},
+  data   : {regra : '##/##/####'},
+  placa  : {regra : '$$$-####'},
+  cnpj   : {regra : '##.###.###/####-##'}
 };
 
+document.addEventListener('DOMContentLoaded', function(evento){
 
-window.addEventListener('keyup', function(evento) {
-  var alvo = evento.target;
-  var masc = alvo.getAttribute('data-mascara');
+  window.addEventListener('keyup', function(evento) {
+    var alvo = evento.target;
+    var masc = alvo.getAttribute('data-mascara');
 
-  if (masc) {
-    mascara(alvo, masc);
-  }
+    if (masc) {
+      mascara(alvo, masc);
+    }
+  });
 });
+
 
 
 function mascara(input, atributo){
   var valor = input.value;
   var regra = padrao[atributo]['regra'];
-  var tipo  = padrao[atributo]['tipo'];
 
   input.setAttribute('maxlength', regra.length);
-
-  if (tipo == 'numero') {
-    valor = valor.replace(/[A-Z]/gi, '');
-  }
-  else if (tipo == 'texto') {
-    valor = valor.replace(/[0-9]/gi, '');
-  }
 
   var valorNovo = '';
 
   if (valor != '') {
+
     for (i = 0; i < valor.length; i++) {
 
-      if (regra[i] != '#' && valor[i] != regra[i]) {
-        valorNovo = valorNovo + regra[i];
+      if (regra[i] == '#') {
+        //valor[i] = valor[i].replace(/[^0-9]/gi, '');
+        valorNovo += valor[i];
       }
-
-      valorNovo = valorNovo + valor[i];
+      else if (regra[i] == '$') {
+        //valor[i] = valor[i].replace(/[^A-Z]/gi, '');
+        valorNovo += valor[i];
+      }
+      else if (regra[i] == '%') {
+        //valor[i] = valor[i].replace(/[^0-9A-Z]/gi, '');
+        valorNovo += valor[i];
+      }
+      else if (regra[i] == valor[i]) {
+        valorNovo += valor[i];        
+      }
+      else if (regra[i] != valor[i]) {
+        valorNovo += regra[i] + valor[i];
+      }
     }
+    
+    input.value = valorNovo;
   }
-
-  input.value = valorNovo;
 }
-
-//BUGS e MELHORIAS E TESTES//
-
-//1- SE COPIAR ALGO COM MAIS CARACTERES DOQUE O PERMITIDO PELO MAXLENGTH, E COLAR NO INPUT, VAI DAR ERRO.
-//2- MELHORAR OBJETO "PADRAO"
-//3- TESTAR DE TODAS AS FORMAS, COM O INTUITO DE BUGAR, PARA APÓS ISSO CORRIGIR.
